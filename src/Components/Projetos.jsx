bc096e67034bc3d000c3cae55389cf0527e1e59c
Marcelo De Lima <delima_marcelo@outlook.com>
2022-12-15 19:07:58 -0300

    feat: desenvolvendo arquivo de projetos

--- src/Components/Projetos.jsx
+++ src/Components/Projetos.jsx
@@ -1,7 +1,21 @@
 import React from 'react';
+import projetos from '../data/projetos'
 
 function Projetos() {
-  return <h1>projetos</h1>;
+
+  if (!projetos.length) return <h3>Carregando...</h3>
+
+  return (
+    <div>
+        {projetos.map((item, index) => (
+            <div key={index + 1}>
+                <a href={item.link} target="_blank">
+                <h1>{item.name}</h1>
+                </a>
+            </div>
+        ))}
+    </div>
+  );
 }
 
 export default Projetos;
---
+++ src/data/projetos.js
@@ -0,0 +1,10 @@
+const projetos = [
+  { name: "CarShop", link: 'https://github.com/Maarceloo/car-shop' },
+  { name: "Trybe Futebol Clube", link: 'https://github.com/Maarceloo/trybe-futebol-clube' },
+  { name: "trivia Game", link: "https://github.com/Maarceloo/trivia-game" },
+  { name: "testes em React", link: 'https://github.com/Maarceloo/testes-em-react' },
+  { name: "FrontEnd online store", link: 'https://github.com/Maarceloo/frontend-online-store'},
+  { name: "BlogsApi", link: 'https://github.com/Maarceloo/blogs-api'},
+];
+
+export default projetos;
