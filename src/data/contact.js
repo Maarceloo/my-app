e1437fba4fa977cdb37432834a1cf88bb3a468c5
Marcelo De Lima <delima_marcelo@outlook.com>
2022-12-15 18:38:28 -0300

    feat: database

---
+++ src/data/contact.js
@@ -0,0 +1,17 @@
+const contact = [
+    {
+      img: "https://img.shields.io/badge/-GITHUB-111111?style=for-the-badge&logo=github&logoColor=white",
+      link: "https://github.com/Maarceloo",
+    },
+    {
+      img: "https://img.shields.io/badge/-LinkedIn-%230077B5?style=for-the-badge&logo=linkedin&logoColor=white",
+      link: "https://www.linkedin.com/in/developer-marcelo-de-lima/",
+    },
+    {
+      img: "https://img.shields.io/badge/Microsoft_Outlook-0078D4?style=for-the-badge&logo=microsoft-outlook&logoColor=white",
+      link: "mailto:delima_marcelo@outlook.com",
+    }
+  ];
+  
+  export default contact;
+  
