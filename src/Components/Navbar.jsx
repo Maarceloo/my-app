3ce4b92461a50ed5121e3ab56ae65b813bc9b538
Marcelo De Lima <delima_marcelo@outlook.com>
2023-01-20 13:41:11 -0300

    Feat: nav bar finalizada

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,24 +1,57 @@
 import React from "react";
-import Link from "@mui/material/Link";
 
 function Navbar() {
   return (
-    <div className="nav">
-      <h1 className="name">Marcelo de Lima</h1>
-      <div className="navbar">
-        <Link href="#about" underline="none">
-          Sobre
-        </Link>
-        <Link href="#skills" underline="none">
-          My Skills
-        </Link>
-        <Link href="#projects" underline="none">
-          Projetos
-        </Link>
-        <Link href="#contact" underline="none">
-          Contato
-        </Link>
-      </div>
+    <div class="container" id="nav-container">
+      <nav class="navbar navbar-expand-lg bg-body-tertiary">
+        <div class="container-fluid">
+          <button
+            class="navbar-toggler"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#navbarTogglerDemo01"
+            aria-controls="navbarTogglerDemo01"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span class="navbar-toggler-icon"></span>
+          </button>
+          <div class="collapse navbar-collapse" id="navbarTogglerDemo01">
+            <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+              <li class="nav-item">
+                <a class="nav-link active" aria-current="page" href="#sobre">
+                  Sobre
+                </a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link active" aria-current="page" href="#skills">
+                  My Skills
+                </a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link active" aria-current="page" href="#projetos">
+                  Projetos
+                </a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link active" aria-current="page" href="#contato">
+                  Contato
+                </a>
+              </li>
+            </ul>
+            <nav class="navbar bg-body-tertiary">
+              <div class="container">
+                  <img
+                    src="https://cdn-icons-png.flaticon.com/512/57/57049.png"
+                    alt="Foto Perfil"
+                    width="30"
+                    height="24"
+                  />
+              </div>
+            </nav>
+          </div>
+        </div>
+      </nav>
     </div>
   );
 }
